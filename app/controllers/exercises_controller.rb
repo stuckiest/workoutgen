896f4a2321c2b8b694345e88874b7c45ad0030f1
stuckiest <stuckiest@gmail.com>
2016-03-08 19:01:43 -0700

    add edit and new form

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -15,7 +15,7 @@ class ExercisesController < ApplicationController
   def create
     @exercise = Exercise.new(exercise_params)
     if @exercise.save
-      redirect_to exercise_path
+      redirect_to exercises_path
     else
       render :new
     end    
